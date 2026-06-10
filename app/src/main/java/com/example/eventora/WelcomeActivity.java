2304bc39f2853bafe21689e897f35b15bc8c084b
purvak2878 <purvakulkarni456@gmail.com>
2026-06-11 00:03:35 +0530

    Added the Login activity

--- app/src/main/java/com/example/eventora/WelcomeActivity.java
+++ app/src/main/java/com/example/eventora/WelcomeActivity.java
@@ -1,12 +1,26 @@
 package com.example.eventora;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Button btnGetStarted = findViewById(R.id.btnGetStarted);
+
+        btnGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
