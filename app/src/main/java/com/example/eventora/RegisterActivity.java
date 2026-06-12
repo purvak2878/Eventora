3171774bccf7d74c92a3061d439d4781fa06b2c9
purvak2878 <purvakulkarni456@gmail.com>
2026-06-12 20:58:46 +0530

    Added the Register activity

--- app/src/main/java/com/example/eventora/RegisterActivity.java
+++ app/src/main/java/com/example/eventora/RegisterActivity.java
@@ -6,18 +6,20 @@ import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
-        TextView txtNewStudent = findViewById(R.id.txtNewStudent);
-        txtNewStudent.setOnClickListener(new View.OnClickListener() {
+        TextView txtSignIn = findViewById(R.id.txtSignIn);
+        txtSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                 startActivity(intent);
+                finish(); // Optional: close register page so back button doesn't go back to it
             }
         });
     }
